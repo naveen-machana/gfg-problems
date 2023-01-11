dace470a2ec0640c7794a10a552a437c0cf2b183
Naveen Machana <naveen.machana@gmail.com>
2023-01-11 11:53:14 +0530

    generate shortest possible codes for station names

---
+++ src/main/java/com/naveen/gfgproblems/trie/GenerateShortestPossibleCodesForStations.java
@@ -0,0 +1,41 @@
+package com.naveen.gfgproblems.trie;
+
+import java.util.TreeMap;
+
+// https://practice.geeksforgeeks.org/batch/dsa-4/track/DSASP-Trie/problem/renaming-cities28581833
+public class GenerateShortestPossibleCodesForStations {
+    private static class Node {
+        boolean isEndOfWord;
+        TreeMap<Character, Node> mp;
+        public Node() {
+            isEndOfWord = false;
+            mp = new TreeMap<>();
+        }
+    }
+    public static void check(String cities[],int n)
+    {
+        Node root = new Node();
+        for (String city : cities) {
+            Node cur = root;
+            StringBuilder sb = new StringBuilder();
+            boolean appendable = true;
+
+            for (char c : city.toCharArray()) {
+                if (appendable) {
+                    sb.append(c);
+                }
+                if (cur.mp.get(c) == null) {
+                    cur.mp.put(c, new Node());
+                    appendable = false;
+                }
+                cur = cur.mp.get(c);
+            }
+            String code = sb.toString();
+            if (code.equals(city) && cur.isEndOfWord) {
+                code = code + " 2";
+            }
+            cur.isEndOfWord = true;
+            System.out.println(code);
+        }
+    }
+}
