1ba0d020615f01ae8f2ba3d231ce736064b2ad9c
Naveen Machana <naveen.machana@gmail.com>
2023-01-21 10:50:11 +0530

    nth catalan number

--- src/main/java/com/naveen/gfgproblems/dp/CountBSTWithNKeys.java
+++ src/main/java/com/naveen/gfgproblems/dp/CountBSTWithNKeys.java
@@ -1,4 +1,36 @@
 package com.naveen.gfgproblems.dp;
 
+// given n - number of keys, how many different BST we can form with n keys
+// if n = 0 => 1 , which is 1 empty BST
+// if n = 1 => 1, BST with 1 key
+// if n = 2 => 2, one root, one key on either left or right
+// if n = 3 => 5
+//     1          1           2        3       3
+//       2          3       1   3     2     1
+//         3      2                  1        2
 public class CountBSTWithNKeys {
+
+    int count(int n) {
+        if (n <= 2) return n;
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            res += count(i - 1) * count(n - i);
+        }
+        return res;
+    }
+
+    int countdp(int n) {
+        if (n <= 1) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1; dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += (dp[j - 1] * dp[i - j]);
+            }
+        }
+
+        return dp[n];
+    }
 }
--- src/main/java/com/naveen/gfgproblems/dp/EggDrop.java
+++ src/main/java/com/naveen/gfgproblems/dp/EggDrop.java
@@ -1,10 +1,18 @@
 package com.naveen.gfgproblems.dp;
 
 // minimum number of drops required to findout the threshold floor where egg starts breaking
+// for every floor, optimal trials required is calculated by looping through every floor before it (including that floor)
+// at every floor there are 2 cases, either egg breaks or it wont
+// if egg breaks, then we have to find out where the egg breaks before this floor recursively
+// if egg wont break, then we have n - f floors to check, which are after this floor
+// adding 1 to result is because, 1 trail has been made
+// taking the maximum because egg might break at all floors (otherway of saying it is, egg might break from 1st floor,
+// so if we start from say floor 5, we would have tried all floors, like 4,3,2,1.
+// taking the minimum because, we have to find out the optimal solution
 public class EggDrop {
 
     public static void main(String[] args) {
-        System.out.println(mindropsdp(3, 16));
+        System.out.println(mindropsdp(2, 4));
     }
     int mindrops(int e, int f) {
         if (e == 1) return f;
